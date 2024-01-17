0cc5c0f058bc12b0bde480e64df3c5043b7002af
Chris King-Parra <chris@kingparra.work>
2024-01-17 16:26:50 -0500

    Add boilerplate to project.sh

--- src/project.sh
+++ src/project.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+IFS=$'\0' # only split strings on ASCII NUL during parameter expansion, breaks some commands
+set -o nounset # error when expanding unset variables
+set -o errexit # exit immediately if a command returns non-zero
+set -o errtrace # generate ERR event on error, so handlers like 'trap $cmd ERR' commands will run, useful for logging
+
 echo "Welcome to our project!"
 echo "Not implemented." >&2
 exit 1
